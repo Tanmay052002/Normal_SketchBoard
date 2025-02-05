1c2645681e2830b53207b06a5d714053c7aeb364
Tanmay Bansal <tanmaybansal441@gmail.com>
2025-02-05 22:27:21 +0530

    updated server.js

--- server.js
+++ server.js
@@ -1,27 +1,60 @@
-const express = require('express');
-const http = require('http');
-const socketIo = require('socket.io');
+const express = require("express");
+const http = require("http");
+const socketIo = require("socket.io");
 
 const app = express();
 const server = http.createServer(app);
 const io = socketIo(server);
 
-app.use(express.static('public')); // Serve static files like HTML, JS, CSS
+let undoStack = []; // To keep track of drawing points for all users
 
-io.on('connection', (socket) => {
-    console.log('a user connected');
+// Serve static files from the "public" directory
+app.use(express.static("public"));
 
-    // Listen for drawing data from the client
-    socket.on('draw', (data) => {
-        // Broadcast drawing data to all connected clients
-        socket.broadcast.emit('draw', data);
+// Serve index.html
+app.get("/", (req, res) => {
+    res.sendFile(__dirname + "/index.html");
+});
+
+// On new client connection
+io.on("connection", (socket) => {
+    console.log("A user connected");
+
+    // Send the current undo stack (drawing state) to the new client
+    socket.emit("initialize", undoStack);
+
+    // Handle mousedown event (start drawing)
+    socket.on("mousedown", (point) => {
+        undoStack.push(point); // Save the point for undo
+        socket.broadcast.emit("mousedown", point); // Broadcast to other clients
     });
 
-    socket.on('disconnect', () => {
-        console.log('user disconnected');
+    // Handle mousemove event (drawing in progress)
+    socket.on("mousemove", (point) => {
+        undoStack.push(point); // Save the point for undo
+        socket.broadcast.emit("mousemove", point); // Broadcast to other clients
+    });
+
+    // Handle undo event
+    socket.on("undo", () => {
+        if (undoStack.length > 0) {
+            undoStack.pop(); // Remove the last drawing point for undo
+            io.emit("undo"); // Broadcast undo action to all clients
+        }
+    });
+
+    // Handle redo event (implement similar to undo if necessary)
+    socket.on("redo", () => {
+        // Add redo functionality if needed
+    });
+
+    // Handle client disconnect
+    socket.on("disconnect", () => {
+        console.log("A user disconnected");
     });
 });
 
+// Start the server on port 3000
 server.listen(3000, () => {
-    console.log('Server running on http://localhost:3000');
+    console.log("Server is running on http://localhost:3000");
 });
